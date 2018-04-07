2baaba3a86b78697e2e526ad00a2e7fe71fddfaf
John Win <john.win@udemy.com>
2018-04-06 19:29:30 -0700

    Add OSX settings.

---
+++ osx/install.sh
@@ -0,0 +1,9 @@
+# Set sidebar icon size to small
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
+# Set fast keyboard repeat rate
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+defaults write NSGlobalDomain KeyRepeat -int 3
+
+# Use list view in all Finder windows by default
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
