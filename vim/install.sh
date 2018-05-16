56608f1222dcda8bcb8e3c6e77c3b5585e5943d0
John Win <john.win@udemy.com>
2018-05-16 00:37:27 -0700

    Tweak install script.

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,13 @@
-ln -s ~/Code/dotfiles/vim ~/.vim
-ln -s ~/Code/dotfiles/vim/vimrc ~/.vimrc
+#!/bin/sh
+
+# clean up
+rm ~/.vim
+rm ~/.vimrc
 rm -rf bundle
+
+# install stuff
+ln -s ~/Code/dotfiles/vim/ ~/.vim
+ln -s ~/Code/dotfiles/vim/vimrc ~/.vimrc
 mkdir -p bundle
 git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
 vim +PluginInstall +qall
