a7348d85560c2ab250344bf09d91a36069f414b6
John Win <john.win@udemy.com>
2018-05-16 00:26:27 -0700

    Use Vundle.

--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,6 @@
 ln -s ~/Code/dotfiles/vim ~/.vim
 ln -s ~/Code/dotfiles/vim/vimrc ~/.vimrc
+rm -rf bundle
+mkdir -p bundle
+git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
+vim +PluginInstall +qall
